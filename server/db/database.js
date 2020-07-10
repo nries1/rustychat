3357a0079490ed862aed156fce67f675437c03c9
Nicolas Ries <nries1@gmail.com>
2020-07-10 16:27:38 -0400

    fixed connection string on heroku

--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require(`sequelize`);
 const dotenv = require('dotenv').config().parsed;
 console.log(`Running on ${ process.env.PLATFORM || 'macOS'}`)
-const connectionString = process.env.PLATFORM.trim() === 'windows' ? `postgres://${dotenv.PSQL_USER}:${dotenv.PSQL_PW}@127.0.0.1:5432/twitchtrivia` : `postgres://localhost:5432/twitchtrivia`;
+const platform = process.env.PLATFORM ? process.env.PLATFORM.trim() : null
+const connectionString = platform === 'windows' ? `postgres://${dotenv.PSQL_USER}:${dotenv.PSQL_PW}@127.0.0.1:5432/twitchtrivia` : `postgres://localhost:5432/twitchtrivia`;
 const dbUrl =
 process.env.DATABASE_URL || connectionString;
 const db = new Sequelize(dbUrl, {
