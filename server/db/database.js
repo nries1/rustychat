61027c7e3f824e0aa3e3d915a35627cfa7aae960
Nicolas Ries <nries1@gmail.com>
2020-07-11 15:13:41 -0400

    logging env

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,7 @@
 const Sequelize = require(`sequelize`);
 const dotenv = require('dotenv').config().parsed;
+console.log('DB dotenv = ', dotenv);
+console.log('DB process.env = ', process.env);
 console.log(`Running on ${ process.env.PLATFORM || 'macOS'}`)
 const platform = process.env.PLATFORM ? process.env.PLATFORM.trim() : null
 const connectionString = platform === 'windows' ? `postgres://${dotenv.PSQL_USER}:${dotenv.PSQL_PW}@127.0.0.1:5432/twitchtrivia` : `postgres://localhost:5432/twitchtrivia`;
